71b107a0156dd17757057e6fcdd65cd8509f4a31
Graeme Gregory <graeme@nuviainc.com>
2020-08-28 18:03:35 +0100

    qemu/qemu_sbsa: add support for sbsa-ref Embedded Controller
    This allows PSCI in TF-A to signal platform power states to QEMU
    via a controller in secure space.
    
    This required a sbsa-ref specific version of PSCI functions for the
    platform. Also adjusted the MMU range to also include the new EC.
    
    Signed-off-by: Graeme Gregory <graeme@nuviainc.com>
    Change-Id: Iece8a88947f11e82ab8988e460a8a66ad175a5ee

--- plat/qemu/qemu_sbsa/include/platform_def.h
+++ plat/qemu/qemu_sbsa/include/platform_def.h
@@ -198,9 +198,9 @@
 /* This is map from GIC_DIST up to last CPU (255) GIC_REDISTR */
 #define DEVICE0_BASE			0x40000000
 #define DEVICE0_SIZE			0x04080000
-/* This is map from NORMAL_UART up to SECURE_UART_MM */
-#define DEVICE1_BASE			0x60000000
-#define DEVICE1_SIZE			0x00041000
+/* This is map from SECURE_EC up to SECURE_UART_MM */
+#define DEVICE1_BASE			0x50000000
+#define DEVICE1_SIZE			0x10041000
 
 /*
  * GIC related constants
